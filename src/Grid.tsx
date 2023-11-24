1eb316026f7abae7cdb1ec6f29c0a083c78f8285
Diego Jimenez <dijim45@gmail.com>
2023-11-24 07:08:59 -0500

    TASK #1: add title

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -14,18 +14,17 @@ const columnDefs: ColDef[] = [
   { field: "period_yr", headerName: "Period (yr)" },
   { field: "i_deg", headerName: "Inclination (deg)" },
   { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
   return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <AgGridReact
-        rowData={data}
-        columnDefs={columnDefs}
-        rowGroupPanelShow={'always'}
-      />
-    </div>
+    <>
+      <h1>Near-Earth Object Overview</h1>
+      <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+        <AgGridReact rowData={data} columnDefs={columnDefs} rowGroupPanelShow={"always"} />
+      </div>
+    </>
   );
 };
 
