90061166d1ab9e3692f0998e38643c189407b372
Diego Jimenez <dijim45@gmail.com>
2023-11-24 08:57:43 -0500

    TASK #2: handle columns sort-filter

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,20 +1,33 @@
 import { AgGridReact } from "ag-grid-react";
 import { ColDef } from "ag-grid-community";
+import { dateCompare } from "./utils/dates";
+
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+// convert number values to be able to filter them correctly
+const formattedData = data.map((obj) => ({
+  ...obj,
+  h_mag: obj.h_mag && Number(obj.h_mag),
+  moid_au: obj.moid_au && Number(obj.moid_au),
+  q_au_1: obj.q_au_1 && Number(obj.q_au_1),
+  q_au_2: obj.q_au_2 && Number(obj.q_au_2),
+  period_yr: obj.period_yr && Number(obj.period_yr),
+  i_deg: obj.i_deg && Number(obj.i_deg),
+}));
+
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
+  { field: "discovery_date", headerName: "Discovery Date", filter: "agDateColumnFilter", comparator: dateCompare },
+  { field: "h_mag", headerName: "H (mag)", filter: "agNumberColumnFilter" },
+  { field: "moid_au", headerName: "MOID (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_1", headerName: "q (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_2", headerName: "Q (au)", filter: "agNumberColumnFilter" },
+  { field: "period_yr", headerName: "Period (yr)", filter: "agNumberColumnFilter" },
+  { field: "i_deg", headerName: "Inclination (deg)", filter: "agNumberColumnFilter" },
   { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
+  { field: "orbit_class", headerName: "Orbit Class" },
 ];
 
 const NeoGrid = (): JSX.Element => {
@@ -22,7 +35,12 @@ const NeoGrid = (): JSX.Element => {
     <>
       <h1>Near-Earth Object Overview</h1>
       <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-        <AgGridReact rowData={data} columnDefs={columnDefs} rowGroupPanelShow={"always"} />
+        <AgGridReact
+          defaultColDef={{ sortable: true, unSortIcon: true, filter: true }}
+          rowData={formattedData}
+          columnDefs={columnDefs}
+          rowGroupPanelShow={"always"}
+        />
       </div>
     </>
   );
